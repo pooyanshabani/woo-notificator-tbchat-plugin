016724c4083bcb6071934a6c764aac853e52da71
pooyan shabani <36469921+pooyanshabani@users.noreply.github.com>
2025-04-06 22:06:11 +0330

    Update wtnp_outof_stock.php

--- inc/wtnp_outof_stock.php
+++ inc/wtnp_outof_stock.php
@@ -2,8 +2,8 @@
 defined('ABSPATH') || exit;
 
 
-add_action( 'woocommerce_low_stock', 'custom_low_stock_action');
-function custom_low_stock_action( $product ) {
+add_action( 'woocommerce_no_stock', 'wtnp_custom_outof_stock_action');
+function wtnp_custom_outof_stock_action( $product ) {
 
     $current_time = date_i18n('l j F Y - H:i');
 	$product_link = $product->get_permalink();
@@ -11,25 +11,23 @@ function custom_low_stock_action( $product ) {
 	$product_name = $product->get_name();
 	$stock_quantity = $product->get_stock_quantity();
 
-	$wtnp_message = "⚠️ موجودی محصول کم شده است!" . "\n";
+	$wtnp_message = "⚠️ موجودی محصول تمام شده است!" . "\n";
 	$wtnp_message .= "🕒 زمان: $current_time" . "\n";
 	$wtnp_message .= "📦 محصول: $product_name" . "\n";
 
 	if ($stock_quantity) {$wtnp_message .= "🥡 موجودی: $stock_quantity" . "\n";}
-	$wtnp_message .= "\n" . "$product_link" . "\n";
+	//$wtnp_message .= "\n" . "$product_link" . "\n";
+	$wtnp_message .= "#اتمام_موجودی";
 	 
 
     global $wtnp_settings_telegramcb;
 	global $wtnp_settings_teltoken;
-	global $wtnp_settings_balecb;
-	global $wtnp_settings_baletoken;
+
 
     
     if ($wtnp_settings_telegramcb == 'yes' && $wtnp_settings_teltoken) {
-		notificator_send_message_wtnp_telegram($wtnp_message);
+		notificator_send_message_wtnp_telegram($wtnp_message, $product_link);
 	}
-	if ($wtnp_settings_balecb == 'yes' && $wtnp_settings_baletoken) {
-		notificator_send_message_wtnp_bale($wtnp_message);
-	}
+
   
 }
